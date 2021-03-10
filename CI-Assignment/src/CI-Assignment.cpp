20fb0d6b6e7309d30c481ef3f8a12b0e81cfce4d
Björn Johansson <andromeda_stargate@hotmail.com>
2021-03-10 19:36:13 +0100

    added return 0 functions to allow project to build

--- CI-Assignment/src/CI-Assignment.cpp
+++ CI-Assignment/src/CI-Assignment.cpp
@@ -1,4 +1,7 @@
 #include "CI-Assignment.h"
+#include <climits>
+#include <iostream>
+#include <string>
 
 AssignmentMath::AssignmentMath()
 {
@@ -13,9 +16,39 @@ int AssignmentMath::addInt(int a, int b)
 	return a + b;
 }
 
+int AssignmentMath::addInt(char a, int b)
+{
+	return 0;
+}
+
+int AssignmentMath::addInt(int a, char b)
+{
+	return 0;
+}
+
+int AssignmentMath::addInt(char a, char b)
+{
+	return 0;
+}
+
 int AssignmentMath::subInt(int a, int b)
 {
 	return a - b;
+}
+
+int AssignmentMath::subInt(char a, int b)
+{
+	return 0;
+}
+
+int AssignmentMath::subInt(int a, char b)
+{
+	return 0;
+}
+
+int AssignmentMath::subInt(char a, char b)
+{
+	return 0;
 }
 
 int AssignmentMath::multInt(int a, int b)
